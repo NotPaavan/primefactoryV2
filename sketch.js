a394f127101e294ce7f9c367bc3865a1765b00fd
NotPaavan <75799708+NotPaavan@users.noreply.github.com>
2024-03-10 19:33:13 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,6 +6,9 @@ function preload() {
 
 var to = 0;
 var ty = 372.5;
+var bo1 = 0;
+var bo2 = 0;
+var bo3 = 0;
 
 function draw(){ 
   
@@ -39,6 +42,45 @@ function draw(){
   text('™', 230, 660);
   
   if (to > 600) { ty -= 2.5; }
-  if (ty < 150) { ty += 2.5; }
+  if (ty < 150) { 
+    
+    ty += 2.5; 
+    
+    fill(255, 255, 255, bo1);
+    stroke(255);
+    strokeWeight(5);
+    rect(300, 200, 600, 100, 10);
+    textSize(75);
+    fill(148, 226, 255);
+    noStroke();
+    text('PLAY', 475, 275);
+    
+    fill(255, 255, 255, bo2);
+    stroke(255);
+    strokeWeight(5);
+    rect(300, 350, 600, 100, 10);
+    textSize(65)
+    fill(148, 226, 255);
+    noStroke();
+    text('how what huh', 345, 425);
+    
+    fill(255, 255, 255, bo3);
+    stroke(255);
+    strokeWeight(5);
+    rect(300, 500, 600, 100, 10);
+    textSize(75);
+    fill(148, 226, 255);
+    noStroke();
+    text('CREDITS', 400, 575);
+    
+  }
   
+  if (mouseX > 300 && mouseX < 900 && mouseY > 200 && mouseY < 300) { bo1 += 10; }
+  else if (bo1 > 0) { bo1 -= 10; }  else { bo1 = 0; }  
+  
+  if (mouseX > 300 && mouseX < 900 && mouseY > 350 && mouseY < 450) { bo2 += 10; }
+  else if (bo2 > 0) { bo2 -= 10; }  else { bo2 = 0; } 
+  
+  if (mouseX > 300 && mouseX < 900 && mouseY > 500 && mouseY < 600) { bo3 += 10; }
+  else if (bo3 > 0) { bo3 -= 10; }  else { bo3 = 0; } 
 }
